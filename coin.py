abe1469f02d11297d649895f36e1a847bce37c26
Aayush Pande <aayushpande.220@gmail.com>
2026-07-01 20:22:08 -0700

    player.py added

--- coin.py
+++ coin.py
@@ -10,19 +10,16 @@ import random
 
 class Coin:
     def __init__(self):
-        """Initialize the coin with a random side up."""
         if random.randint(0, 1) == 0:
             self.__sideup = "Heads"
         else:
             self.__sideup = "Tails"
 
     def toss(self):
-        """Randomly toss the coin."""
         if random.randint(0, 1) == 0:
             self.__sideup = "Heads"
         else:
             self.__sideup = "Tails"
 
     def get_sideup(self):
-        """Return the current side facing up."""
         return self.__sideup
